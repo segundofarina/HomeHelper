e805624f97d83567cd651a8698f74015e9495691
tinchovictory <martin@victory.com.ar>
2018-05-11 22:05:32 -0300

    profile providerid not found returns exception handled

--- webapp/src/main/java/ar/edu/itba/paw/homehelper/controller/controllerAdvice/ExceptionsControllerAdvice.java
+++ webapp/src/main/java/ar/edu/itba/paw/homehelper/controller/controllerAdvice/ExceptionsControllerAdvice.java
@@ -2,6 +2,7 @@ package ar.edu.itba.paw.homehelper.controller.controllerAdvice;
 
 import ar.edu.itba.paw.homehelper.exceptions.ForbiddenException;
 import ar.edu.itba.paw.homehelper.exceptions.NotFoundException;
+import ar.edu.itba.paw.homehelper.exceptions.ProviderNotFoundException;
 import ar.edu.itba.paw.interfaces.services.SProviderService;
 import ar.edu.itba.paw.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,7 @@ public class ExceptionsControllerAdvice {
 
     @Autowired
     SProviderService sProviderService;
-
+/*
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
     public ModelAndView notFoundHandler(@ModelAttribute("loggedInUser") final User loggedInUser) {
@@ -40,6 +41,19 @@ public class ExceptionsControllerAdvice {
         mav.addObject("userProviderId", sProviderService.getServiceProviderId(getUserId(loggedInUser)));
         mav.addObject("errorNum", "403");
         mav.addObject("errorDesc", "Access Forbidden");
+
+        return mav;
+    }*/
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ProviderNotFoundException.class)
+    public ModelAndView providerNotFound(@ModelAttribute("loggedInUser") final User loggedInUser) {
+        final ModelAndView mav = new ModelAndView("error/404");
+
+        mav.addObject("user", loggedInUser);
+        mav.addObject("userProviderId", sProviderService.getServiceProviderId(getUserId(loggedInUser)));
+        mav.addObject("errorNum", "404");
+        mav.addObject("errorDesc", "Provider Not Found");
 
         return mav;
     }
