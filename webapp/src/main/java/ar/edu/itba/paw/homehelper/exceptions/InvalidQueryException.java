db3f6168741cbb2c2d1246695afe21eb486c6275
Segundo Farina <afarina@itba.edu.ar>
2018-12-06 18:41:44 -0300

    Deleted jsp and old controllers

--- webapp/src/main/java/ar/edu/itba/paw/homehelper/exceptions/InvalidQueryException.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.itba.paw.homehelper.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class InvalidQueryException extends Exception {
-
-}
